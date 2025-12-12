51f1c73158f7052a1c6d5af9d2c0e83975d776f9
topsailRobotics <topsailroboticsteam@gmail.com>
2025-12-12 15:55:08 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,10 @@
 package gitTest1;
 public class hello{
     public static void main (String args[]){
-        System.out.print("Helow world and merry christmas!");
+        int cater = 67;
+        for (int i; i<10; i++){
+            carter ++;
+        }
         System.out.print("I added this in Github!");
     }
 }
