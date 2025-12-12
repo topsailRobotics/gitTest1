305cbd16d97b1da4f02b262d33ce99ada3c7186b
topsailRobotics <topsailroboticsteam@gmail.com>
2025-12-12 15:52:01 -0500

    Fix typo in print statement

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 package gitTest1;
 public class hello{
     public static void main (String args[]){
-        System.out.print("Helow World!");
+        System.out.print("Helow World");
         System.out.print("I added this in Github!");
     }
 }
